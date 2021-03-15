dd510fc51b2b19595b3855c62e30e9fb2ae73ed0
Delray Frierson <demdel98@gmail.com>
2021-03-15 01:24:11 +0000

    function 1,2,3,4,5

--- main.fs
+++ main.fs
@@ -3,43 +3,77 @@ module Assignment
 // Problem 1
 let rec tribn n =
     // write your code here
-    if n = 0 then 0 
+    if n <= 0 then 0 
     else if n =  1 then 1
     else if n =  2 then 1
     else tribn (n-1) + tribn (n-2) + tribn (n-3)
     
 // Problem 2
 let tribn2 n =
-    // write your code here
-  0
+    // write your code here 
+    if n <= 0 then 0 
+    else 
+      let rec Tfour lst = 
+        let h = List.head lst
+        1 + h ::(h + 2::( h + 3:: (h + 4 ::[])))::lst
+    
+      let rec tribn n =
+        if n <= 0 then 0 
+        else if n =  1 then 1
+        else if n =  2 then 1
+        else tribn (n-1) + tribn (n-2) + tribn (n-3)
+
+  //0 default
 
 // Problem 3
 let rec last lst =
     // write your code here
-    if last lst > 5 then 0
-    else List.Head :: last lst (List.tail lst)
-
+    if List.isEmpty lst then 0
+    else
+     let h = List.head lst
+     let t = List.tail lst 
+     if h <> last t then h :: t
+     else last h
+  
+  
+  //0 default
     
 
 // Problem 4
 let fourth (lst:int list) =
     // write your code here
-    if lst.Tail < 4 then 0
-    else lst.Tail[4]
-
+    if lst < 4 then 0
+    else 
+        let 
+  
+  
+  
+0 
 // Problem 5
 let everyfourth (lst:int list) =
     // write your code here
-    let rec append (lst: int list) (lst2: int list) = 
-     if lst.isEmpty then lst
-     else lst.Tail[4] :: append lst.Tail[4] lst2
+     
+      if List.isEmpty then []
+      else 
 
+      let fourth (lst:int list) =
+      
+    else  
+      let h = List.head 
+      let t = List.tail
+      if h 
+
+    
+    
+    
 // Problem 6
 let rec take n (lst:int list) =
     // write your code here
-    []
+    
+    [] 
 
 // Problem 7
 let rec drop n (lst:int list) =
     // write your code here
-    []
+    
+    [] 
